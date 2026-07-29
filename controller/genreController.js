b1b26c63d1be1dbaa9502a27f609623f5fa33d7b
riswandaP <riswandaputra03@gmail.com>
2026-07-29 10:38:53 +0700

    Membuat genreController untuk GET data by ID

--- controller/genreController.js
+++ controller/genreController.js
@@ -10,6 +10,20 @@ async function getAllGenre(req, res) {
     }
 }
 
+async function getGenreByID(req, res) {
+    const { id } = req.params;
+    try {
+        const genre = await db.Genre.findByPk(id);
+        if (!genre) {
+            return res.status(404).json({ error: 'Genre not found' });
+        }
+        res.status(200).json(genre);
+    } catch (err) {
+        console.error('Error fetching Genre by ID:', err.message);
+        res.status(500).json({ error: 'Failed to fetch Genre by ID' });
+    }
+}
+
 module.exports = {
     
 }
